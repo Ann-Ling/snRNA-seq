0b5a75c508388f8c3c2f015d50d6fa45fd61e8f5
Ann-Ling <arecolineliu@gmail.com>
2025-04-05 23:00:12 +0800

    Update 03Pseudo-cell generation.sh

--- 02_Cross-species/03Pseudo-cell generation.sh
+++ 02_Cross-species/03Pseudo-cell generation.sh
@@ -1,1 +1,8 @@
+# Combine the UMI matrix of KC between Mpha and Amel
+python universal.combine.KC.umi.py Dros.midgut.umi.geneMatrix.txt AM.umi.geneMatrix.txt Dros_AM.ort.genes Dros AM > Dros_AM.midgut.umi.geneMatrix.txt
 
+cat Dros.pheno.cell.txt AM.pheno.cell.txt | grep -v 'Sample_ID'|sed '1iSample_ID\tStudy_ID\tCelltype' > Dros_AM.midgut.pheno.cell.txt
+
+# Generate pseudo cell umi Matrix
+## we will get：merged.pheno.cell.txt, merged.exp.txt, and merged.umi.txt
+perl random.combine.pl Dros_AM.midgut.umi.geneMatrix.txt Dros_AM.midgut.pheno.cell.txt
